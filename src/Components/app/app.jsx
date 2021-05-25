287cf35646261f4ba6f24cef333cf239b1a19e55
Jyoti Kumari <jyoti02.nitt@gmail.com>
2021-05-25 23:26:15 +0530

    header component updating

---
+++ src/Components/app/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './../Header/Header.jsx';
+import './app.css';
+
+
+// Class Component
+class App extends React.Component
+{
+    render()
+    {
+        return (
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+    
+}
+
+
+
+export default App;
